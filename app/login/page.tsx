d0f8ef245fbac2f552fb7fe8686981a63f10b0c4
matrSid <siddhantj972@gmail.com>
2026-03-18 02:20:51 +0530

    add auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,8 +5,11 @@ export default function LoginPage() {
   const handleGoogleLogin = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
-      options: {    redirectTo: `${window.location.origin}`,  },
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`, // ← add /auth/callback
+      },
     });
+    
     if (error) console.error("Error with Google login:", error.message);
   };
 
